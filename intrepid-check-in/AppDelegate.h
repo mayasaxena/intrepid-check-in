f45efd4c46b534c716370389d58ca431eb8307a6
Maya Saxena <maya.saxena@gmail.com>
2015-06-16 08:36:54 -0400

    Initial Commit

---
+++ intrepid-check-in/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  intrepid-check-in
+//
+//  Created by Maya Saxena on 6/16/15.
+//  Copyright (c) 2015 Intrepid Pursuits. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ intrepid-check-in/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  intrepid-check-in
+//
+//  Created by Maya Saxena on 6/16/15.
+//  Copyright (c) 2015 Intrepid Pursuits. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
