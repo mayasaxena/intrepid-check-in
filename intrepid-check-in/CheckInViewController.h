8a4443953d1f048264c0e0e5d1647c99d83211b6
Maya Saxena <maya.saxena@gmail.com>
2015-06-16 11:37:04 -0400

    Started work on region finding methods

--- intrepid-check-in/CheckInViewController.h
+++ intrepid-check-in/CheckInViewController.h
@@ -10,4 +10,5 @@
 
 @interface CheckInViewController : UIViewController
 
+
 @end
--- intrepid-check-in/CheckInViewController.m
+++ intrepid-check-in/CheckInViewController.m
@@ -7,8 +7,14 @@
 //
 
 #import "CheckInViewController.h"
+#import <CoreLocation/CoreLocation.h>
 
-@interface CheckInViewController ()
+@interface CheckInViewController () <CLLocationManagerDelegate>
+@property (strong, nonatomic) CLLocationManager *locationManager;
+@property (strong, nonatomic) CLCircularRegion *intrepidRegion;
+@property CLLocationCoordinate2D intrepidCoordinates;
+@property (nonatomic, readwrite) CLLocationDegrees latitude;
+@property (nonatomic, readwrite) CLLocationDegrees longitude;
 
 @end
 
@@ -16,22 +22,30 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view from its nib.
+    self.latitude = 42.367105;
+    self.longitude = -71.080447;
+    self.intrepidCoordinates = CLLocationCoordinate2DMake(self.latitude, self.longitude);
+    self.intrepidRegion = [[CLCircularRegion alloc]initWithCenter:self.intrepidCoordinates radius:50.0 identifier:@"Intrepid Pursuits"];
+    
+    if ([CLLocationManager authorizationStatus] == kCLAuthorizationStatusNotDetermined) {
+        
+        self.locationManager = [[CLLocationManager alloc] init];
+        self.locationManager.desiredAccuracy = kCLLocationAccuracyBest;
+        self.locationManager.delegate = self;
+        
+        [self.locationManager requestAlwaysAuthorization];
+        [self.locationManager startUpdatingLocation];
+        NSLog(@"%f", [self.locationManager location].coordinate.latitude);
+    }
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)locationManager:(CLLocationManager *)manager didEnterRegion:(CLRegion *)region {
+    NSLog(@"I'm here!");
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+- (IBAction)startMonitoringPressed:(UIButton *)sender {
+    [self.locationManager startMonitoringForRegion:self.intrepidRegion];
 }
-*/
+
 
 @end
