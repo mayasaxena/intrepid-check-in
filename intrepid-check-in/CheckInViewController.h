3b1aa9a11518a4f11e50d9198002205a4cced4b1
Maya Saxena <maya.saxena@gmail.com>
2015-06-16 08:49:35 -0400

    Added check-in view controller files and nib

--- intrepid-check-in/CheckInViewController.h
+++ intrepid-check-in/CheckInViewController.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  CheckInViewController.h
 //  intrepid-check-in
 //
 //  Created by Maya Saxena on 6/16/15.
@@ -8,8 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
-
+@interface CheckInViewController : UIViewController
 
 @end
-
